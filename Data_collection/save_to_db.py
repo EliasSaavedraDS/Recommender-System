2dae6ef4c74c5e89b80e511912d5929004e0f164
Elias Saavedra <elias.saavedra164@gmail.com>
2025-02-15 03:24:47 -0600

    Saved to Sqlite DB

--- Data_collection/save_to_db.py
+++ Data_collection/save_to_db.py
@@ -5,13 +5,25 @@ csv_files = ['Data_collection/DallasDataset.csv','Data_collection/Houston&Austin
 
 df = pd.concat([pd.read_csv(file) for file in csv_files], ignore_index=True)
 
-conn = sqlite3.connect('Sqlite.db')
+with sqlite3.connect('Sqlite.db') as conn:
+    cursor = conn.cursor()
 
-df.to_sql('Fulldataset',conn,if_exists='replace',index=False)
+    #Create a table and csv for the main or full dataset
+    df.to_sql('Fulldataset',conn,if_exists='replace',index=False)
+    df.to_csv('FullDataset.csv',index=False)
 
+    #Create a table and csv for numerical and categorical feature types (exluding the description feature)
+    df_without_description = df.drop(columns=['Description']).copy()
+    df_without_description.to_sql('AttributesTable', conn, if_exists='replace', index=False)
 
-df.to_csv('FullDataset.csv',index=False)
+    #Create a table for the Description column
+    cursor.execute("DROP TABLE IF EXISTS DescriptionTable")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS DescriptionTable AS
+        SELECT "Post ID", Description
+        FROM Fulldataset
+    """)
+    conn.commit()
 
-conn.close()
-
-print("Data has been successfully integrated into SQLite and saved as FullDataset.csv!")
+#Note: Sqlite Database and FullDataset.csv are stored in the notebook/data directory
+print("Data has been successfully integrated into SQLite and saved")
